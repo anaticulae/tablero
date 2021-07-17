1fa3a8f766693878d4b77e8fac85d2497f6172bf
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-07-17 14:51:09 +0200

    test(horizontal): verify bachelor90 page 77 tables

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -13,6 +13,7 @@ import pytest
 import serializeraw
 import utilatest
 
+import tablero.display
 import tablero.features.horizontal
 
 
@@ -38,10 +39,42 @@ def test_table_bachelor90_page76_extract_table():
     assert len(tables) == 1
 
 
-@pytest.mark.xfail(reason='layout parameter changed')
 @utilatest.requires(power.BACHELOR090_PDF)
-def test_table_bachelor90_page77_extract_table():
+def test_table_bachelor90_page77_extract_table(testdir):
     source = power.link(power.BACHELOR090_PDF)
     page = 77
     tables = extract_tables(source, page)
     assert len(tables) == 3
+    tables = [iamraw.PageContentTableBounding(page=page, content=tables)]
+    tablero.display.render_tables(tables, power.BACHELOR090_PDF, testdir.tmpdir)
+
+
+@pytest.mark.parametrize(
+    'text, line, bounding',
+    [
+        ((0, 15), (0, 5), (203.96, 114.83, 388.33, 214.1)),
+        ((14, 28), (4, 7), (213.93, 295.89, 378.36, 394.89)),
+        ((28, -1), (7, -1), (173.43, 561.47, 418.86, 631.58)),
+    ],
+)
+@utilatest.requires(power.BACHELOR090_PDF)
+def test_table_bachelor90_page77_first_table(text, line, bounding, testdir):
+    source = power.link(power.BACHELOR090_PDF)
+    page = 77
+    ptn = serializeraw.create_pagetextnavigators_frompath(
+        source,
+        pages=(page,),
+    )[0]
+    # shrink navigator data which belongs to the first table
+    ptn.data = ptn.data[text[0]:text[1]]
+    lines = serializeraw.load_lines(
+        iamraw.path.line(source),
+        pages=(page,),
+    )
+    lines = lines[0]
+    lines = lines.content[line[0]:line[1]]
+    tables = tablero.features.horizontal.cluster_page(ptn, lines)
+    tables_bounding = [table.bounding for table in tables]
+    assert tables_bounding == [bounding]
+    tables = [iamraw.PageContentTableBounding(page=page, content=tables)]
+    tablero.display.render_tables(tables, power.BACHELOR090_PDF, testdir.tmpdir)
