cd876ab79e78870e3695df9a4eade7deadf53a9d
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-04-24 18:39:36 +0200

    chore(resources): extend test data generator
    + adjust unit test

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,7 +30,7 @@ RESOURCES = [
     (power.MASTER098_PDF, '53:61'),
     (power.BACHELOR090_PDF, '76:81'),
     (power.MASTER075_PDF, '10:20'),
-    (power.BACHELOR109_PDF, '0:10'),
+    power.BACHELOR109_PDF,
     power.DOCU007_PDF,
     (power.BACHELOR063_PDF, '24:28'),
     (power.MASTER112_PDF, '110'),
