f68bcb71ba4f07cf6e794acba54eb88d676616a2
Helmut Konrad Fahrendholz <helmutus@outlook.com>
2022-09-25 18:56:49 +0200

    feat(ghost): patch backend

--- tablero/__patch__.py
+++ tablero/__patch__.py
@@ -9,10 +9,12 @@
 
 import sys
 
+import camelot.backends.ghostscript_backend
 import camelot.handlers
 import camelot.parsers.lattice
 import PIL
 import PIL.ImageDraw
+import utila
 
 
 def __init__(self, filepath, pages="1", password=None):
@@ -23,7 +25,7 @@ def __init__(self, filepath, pages="1", password=None):
         self.password = password
         if sys.version_info[0] < 3:
             self.password = self.password.encode("ascii")
-    self.pages = self._get_pages(self.filepath, pages)  # pylint:disable=W0212
+    self.pages = self._get_pages(pages)  # pylint:disable=W0212
 
 
 # disable path check, we know what we do.
@@ -51,3 +53,25 @@ def _generate_table_bbox(self):
 
 
 camelot.parsers.lattice.Lattice._generate_table_bbox = _generate_table_bbox  # pylint:disable=W0212
+
+
+def convert(self, pdf_path, png_path, resolution=300):
+    if not self.installed():
+        raise OSError(
+            "Ghostscript is not installed. You can install it using the instructions"
+            " here: https://camelot-py.readthedocs.io/en/master/user/install-deps.html"
+        )
+    gs_command = [
+        "gswin64c",
+        "-q",
+        "-sDEVICE=png16m",
+        "-o",
+        png_path,
+        f"-r{resolution}",
+        pdf_path,
+    ]
+    cmd = ' '.join(gs_command)
+    utila.run(cmd)
+
+
+camelot.backends.ghostscript_backend.GhostscriptBackend.convert = convert
