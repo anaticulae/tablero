9e4da0ae6fa82cf6ebce4f76f6d5e35a64558992
Helmut Konrad Schewe <helmutus@outlook.com>
2026-07-24 22:28:08 +0200

    refactor(patch): disable debug renderer

--- tablero/__patch__.py
+++ tablero/__patch__.py
@@ -17,28 +17,29 @@ import PIL.ImageDraw
 import utilo
 
 
-BEFORE = camelot.parsers.lattice.Lattice._generate_table_bbox  # pylint:disable=W0212
+# BEFORE = camelot.parsers.lattice.Lattice._generate_table_bbox  # pylint:disable=W0212
 
 TODO = None
 
 
-def _generate_table_bbox(self):
-    BEFORE(self)
-    if not TODO:
-        return
-    current = TODO.pop()
-    _, top, __, bottom = [float(item) for item in current.split(',')]
-    top = top * 300 / 72 + 45
-    bottom = bottom * 300 / 72 - 45
-    with PIL.Image.open(self.imagename) as image:
-        draw = PIL.ImageDraw.Draw(image)
-        width, height = image.width + 1, image.height + 1
-        draw.rectangle((0, 0, width, top), fill='white')
-        draw.rectangle((0, bottom, width, height), fill='white')
-        image.save(self.imagename)
+# def _generate_table_bbox(self):
+#     BEFORE(self)
+#     if not TODO:
+#         return
+#     current = TODO.pop()
+#     _, top, __, bottom = [float(item) for item in current.split(',')]
+#     top = top * 300 / 72 + 45
+#     bottom = bottom * 300 / 72 - 45
+#     print(self)
+#     with PIL.Image.open(self.imagename) as image:
+#         draw = PIL.ImageDraw.Draw(image)
+#         width, height = image.width + 1, image.height + 1
+#         draw.rectangle((0, 0, width, top), fill='white')
+#         draw.rectangle((0, bottom, width, height), fill='white')
+#         image.save(self.imagename)
 
 
-camelot.parsers.lattice.Lattice._generate_table_bbox = _generate_table_bbox  # pylint:disable=W0212
+# camelot.parsers.lattice.Lattice._generate_table_bbox = _generate_table_bbox  # pylint:disable=W0212
 
 
 def convert(self, pdf_path, png_path, resolution=300):
